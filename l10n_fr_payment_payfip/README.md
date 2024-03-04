6ebf1fd424a9f8bf6a25c169b5271708acd7b6da
Damien <ultrarushgame@gmail.com>
2024-03-04 14:35:03 +0100

    ADD l10n_fr_payment_payfip for migration

---
+++ l10n_fr_payment_payfip/README.md
@@ -0,0 +1,16 @@
+# Tipiregie payment acquirer
+
+[![pipeline status](https://github.com/Horanet/payment_tipiregie/badges/11.0/pipeline.svg)](https://github.com/Horanet/payment_tipiregie/commits/11.0)
+[![coverage report](https://github.com/Horanet/payment_tipiregie/badges/11.0/coverage.svg)](https://github.com/Horanet/payment_tipiregie/commits/11.0)
+
+## Install Odoo 11
+
+- [Install Odoo 11.0](https://www.odoo.com/documentation/11.0/setup/install.html) on your favorite environment
+
+## Install addon:
+
+- Execute command :
+
+```ruby
+pip install "git+https://github.com/Horanet/payment_tipiregie.git@11.0#egg=odoo11-addon-payment-tipiregie&subdirectory=setup/payment_tipiregie"
+```
---
+++ l10n_fr_payment_payfip/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "Intermédiaire de paiement PayFIP",
+    'version': '16.0.1',
+    'summary': """Intermédiaire de paiement : Implémentation de PayFIP""",
+    'author': "MokaTourisme",
+    'website': "http://www.mokatourisme.fr/",
+    'license': "AGPL-3",
+    'category': 'Accounting',
+    'external_dependencies': {
+        'python': [
+            'openupgradelib',
+        ]
+    },
+    'depends': [
+        'payment',
+        'l10n_fr'
+    ],
+    'qweb': [],
+    'init_xml': [],
+    'update_xml': [],
+    'data': [
+        # Views must be before data to avoid loading issues
+        'views/payment_payfip_templates.xml',
+        'views/payment_views.xml',
+        'data/payment_provider.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'auto_install': False,
+    'installable': True,
+}
