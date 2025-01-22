0d393bad7c2b046399c849c8ce518bd785094c0e
Damien <ultrarushgame@gmail.com>
2025-01-22 09:13:03 +0100

    [ADD] mis_builder

---
+++ l10n_fr_mis_builder/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "L10n FR MIS Builder",
+    "summary": "MIS Builder for French localization",
+    "author": "Moka Tourisme",
+    "website": "https://www.mokatourisme.fr",
+    "category": "Achat",
+    "version": "15.0.1.0.1",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "account",
+        "mis_builder",
+    ],
+    "data": [
+        "data/mis_styles.xml",
+        "data/mis_report.xml",
+        "data/mis_instance.xml", 
+    ],
+}
