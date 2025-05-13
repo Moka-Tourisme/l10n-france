d4f5b751ca7e525c07e55f186ad4498aa76e575f
DUCIEL Romain <romain@mokatourisme.fr>
2025-05-13 16:29:26 +0200

    [ADD] l10n_fr_payment_payfip

---
+++ l10n_fr_payment_payfip/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': "Intermédiaire de paiement PayFIP",
+    'version': '18.0',
+    'summary': """Intermédiaire de paiement : Implémentation de PayFIP""",
+    'author': "MokaTourisme",
+    'website': "http://www.mokatourisme.fr/",
+    'license': "AGPL-3",
+    'category': 'Accounting',
+    'external_dependencies': {
+        'python': [
+            'openupgradelib',
+        ]
+    },
+    'depends': [
+        'payment',
+        'l10n_fr'
+    ],
+    'qweb': [],
+    'init_xml': [],
+    'update_xml': [],
+    'data': [
+        # Views must be before data to avoid loading issues
+        'views/payment_payfip_templates.xml',
+        'views/payment_views.xml',
+        'data/payment_provider_data.xml',
+        'data/payment_transaction.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'auto_install': False,
+    'installable': True,
+}
