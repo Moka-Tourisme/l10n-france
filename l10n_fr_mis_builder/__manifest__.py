58d596fb0d1bf2251bd61fc6b00ed15dfc296580
Damien <ultrarushgame@gmail.com>
2025-06-11 11:40:28 +0200

    [MIG] 18.0 mis_builder

---
+++ l10n_fr_mis_builder/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "L10n FR MIS Builder",
+    "summary": "MIS Builder for French localization",
+    "author": "Moka",
+    "website": "https://www.moka.cloud",
+    "category": "Achat",
+    "version": "18.0.1.0.1",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "account",
+        "mis_builder",
+    ],
+    "data": [
+        "data/mis_styles.xml",
+        "data/mis_report.xml",
+        "data/mis_instance.xml", 
+    ],
+}
